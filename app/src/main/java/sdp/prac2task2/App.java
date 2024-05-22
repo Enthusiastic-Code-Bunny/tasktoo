0e26716bbd55ba485ad3e2f678d554244171271d
Jess <Jessicaackerman02@gmail.com>
2024-05-22 21:26:02 +0200

    Forgot to add dependencies for org.json before last commit

--- app/src/main/java/sdp/prac2task2/App.java
+++ app/src/main/java/sdp/prac2task2/App.java
@@ -9,6 +9,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.json.JSONObject;
 import java.io.File;
 import java.util.Scanner;
 
